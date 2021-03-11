207c78463845bc675037288097736d1000201ad5
Luiggy Macias <sosoteluiggy@gmail.com>
2021-03-11 09:13:50 -0500

    funcion para mostrar el total movida

--- js/script.js
+++ js/script.js
@@ -16,6 +16,9 @@
     var addButtons = document.getElementsByClassName('add');// los botones para añadir al carrito cada item
     var toggle = document.getElementById('toggle');
     var total = 0;
+    var numOfItems = 0;
+    var measure = document.getElementById('measure');
+    measure.innerHTML = numOfItems;
 
 
     for (let i = 0; i < addButtons.length; i++) {// para cada botton crea un evento donde se añadira el precio por cada item
@@ -24,10 +27,18 @@
             let price = items[i].price; //obten el valor del precio correspondiente al item
             prices.push(price); //añade el respectivo precio al array prices
             // console.log(price);
+            numOfItems++;
+            measure.innerHTML = numOfItems;
         });
     }
 
-    showTotal.addEventListener('click', function () {// evento en donde se va a mostrar el total del precio en pantalla
+    showTotal.addEventListener('click', showTotalprice);
+
+    //funciones
+
+
+    //// funcion en donde se va a mostrar el total del precio en pantalla; es llamado cuado se clickea el boton Show total
+    function showTotalprice() {
         for (let i = 0; i < prices.length; i++) {
             const price = prices[i];
             total += price //cada precio en el array precios se suma para obtener el total que se va a mostrar en pantalla
@@ -35,5 +46,5 @@
 
         toggle.innerHTML = '$' + total;
         toggle.className = '';
-    })
+    }
 }())
